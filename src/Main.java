937181c1c7570a6d7f228124cff36128e34ece9d
vitaliy-nykonchuk <vitaliy.nykonchuk@gmail.com>
2023-05-27 20:26:12 +0300

    Add multiplication

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("My git app.");
+        double miles = 15;
+        double kms = 26;
+        double kmsRes = convertMilesToKms(miles);
+        System.out.println(kmsRes + " kms in " + miles + " miles.");
+        double milesRes = convertKmsToMiles(kms);
+        System.out.println(milesRes + " miles in " + kms + " kms.");
+    }
+
+    private static double convertMilesToKms(double miles) {
+        return miles * 1.60934;
+    }
+
+    private static double convertKmsToMiles(double kms) {
+        return kms / 1.60934;
     }
 }
