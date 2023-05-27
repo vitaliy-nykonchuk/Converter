7c7283b2a725aaecb49399485531666440484403
vitaliy-nykonchuk <vitaliy.nykonchuk@gmail.com>
2023-05-27 20:33:01 +0300

    Change app info

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("My git app.");
+        System.out.println("Converter for base converting");
+        System.out.println("Version 1.0");
+        double miles = 15;
+        double kms = 26;
+        double kmsRes = convertMilesToKms(miles);
+        System.out.println(kmsRes + " kms in " + miles + " miles.");
+        double milesRes = convertKmsToMiles(kms);
+        System.out.println(milesRes + " miles in " + kms + " kms.");
+    }
+
+    private static double convertMilesToKms(double miles) {
+        return miles * 1.60934;
+    }
+
+    private static double convertKmsToMiles(double kms) {
+        return kms / 1.60934;
     }
 }
